d34a3e4083de34fb45e997085a672f224f7e41f9
ivoribeiro23 <ivolopesribeiro_15@hotmail.com>
2015-05-27 21:08:54 +0100

    substituiçao da classe gestao tipo salario por gestao genero

--- TrabalhoPP/src/Classes/salario/Salario.java
+++ TrabalhoPP/src/Classes/salario/Salario.java
@@ -5,12 +5,64 @@
  */
 package Classes.salario;
 
+import Classes.ano.Ano;
+import Classes.genero.Genero;
+import Classes.unidade_territorial.UnidadeTerritorial;
 import Exceptions.RegistoSalarioException;
 
 /**
  *
  * @author aluno
  */
-public class Salario extends RegistoSalarioException{
+public class Salario extends RegistoSalarioException {
+
+    private UnidadeTerritorial ut;
+    private Ano ano;
+    private Genero generoSalario;
+    private float salario;
+
+  
+    public Salario(UnidadeTerritorial ut, Ano ano, Genero generoSalario, float salario) {
+        this.ut = ut;
+        this.ano = ano;
+        this.generoSalario = generoSalario;
+        this.salario = salario;
+    }
+
+    public UnidadeTerritorial getUt() {
+        return ut;
+    }
+
+    public void setUt(UnidadeTerritorial ut) {
+        this.ut = ut;
+    }
+
+    public Ano getAno() {
+        return ano;
+    }
+
+    public void setAno(Ano ano) {
+        this.ano = ano;
+    }
+
+    public Genero getGeneroSalario() {
+        return generoSalario;
+    }
+
+    public void setGeneroSalario(Genero generoSalario) {
+        this.generoSalario = generoSalario;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
     
+    
+    
+    
+
 }
